15552d15b6b8fc46614860e5d22e41623f065fde
Tomoya Chiba <tomo.asleep@gmail.com>
2024-05-16 11:01:20 +0900

    Add command line helps

--- lib/yard_to_rbs_inline/cli.rb
+++ lib/yard_to_rbs_inline/cli.rb
@@ -1,6 +1,6 @@
 # rbs_inline: enabled
 
-require 'optparse'
+require "optparse"
 
 module YardToRbsInline
   class Cli
@@ -8,12 +8,24 @@ module YardToRbsInline
       opt = OptionParser.new
       dry_run = false
 
-      opt.on('-n', '--dry-run', 'Dry run') { dry_run = true }
+      opt.banner = <<~STRING
+        Usage: yard-to-rbs-inline [options] <files ...>
+
+      STRING
+
+      opt.on("-n", "--dry-run", "Dry run") { dry_run = true }
       opt.parse!(argv)
+
+      if argv.empty?
+        warn opt.help
+        return 1
+      end
 
       argv.each do |file|
         Converter.convert(file, dry_run: dry_run)
       end
+
+      0
     end
   end
 end
