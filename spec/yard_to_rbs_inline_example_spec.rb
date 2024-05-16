986a8afa96495519b4a256933a5e87788c0d0ca8
Tomoya Chiba <tomo.asleep@gmail.com>
2024-05-16 11:36:11 +0900

    Remove example spec

--- spec/yard_to_rbs_inline_example_spec.rb
+++ spec/yard_to_rbs_inline_example_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe YardToRbsInline do
   it "has a version number" do
     expect(YardToRbsInline::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
